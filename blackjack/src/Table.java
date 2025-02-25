a3b2a1de083fe628f1f484272798ca16b6e92db0
Markylu <46211683+Markylu@users.noreply.github.com>
2025-02-25 15:18:43 -0500

    Made title and ajusted window size

--- blackjack/src/Table.java
+++ blackjack/src/Table.java
@@ -6,7 +6,7 @@ public class Table extends JPanel {
 
     public Table() {
         // set the game board size
-        setPreferredSize(new Dimension(1700,600));
+        setPreferredSize(new Dimension(1200,600));
         // set the game board background color
         setBackground(new Color(232, 232, 232));
 
