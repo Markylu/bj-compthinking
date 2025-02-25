6990a8ae254b02e0e367f0438454e6a1fcc37bdd
Markylu <46211683+Markylu@users.noreply.github.com>
2025-02-25 15:14:48 -0500

    Complete overhaul

---
+++ blackjack/src/Table.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Table extends JPanel {
+
+    public Table() {
+        // set the game board size
+        setPreferredSize(new Dimension(1700,600));
+        // set the game board background color
+        setBackground(new Color(232, 232, 232));
+
+    }
+}
