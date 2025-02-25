612ccec9d5b69645d242aab3d31629c7f890aa22
Markylu <46211683+Markylu@users.noreply.github.com>
2025-02-25 13:59:55 -0500

    Restructured Project

---
+++ blackjack/src/Main.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame frame = new Frame("Basic Program");
+        Label label = new Label("Hello World!");
+
+        // Aligning the label to CENTER
+        label.setAlignment(Label.CENTER);
+
+        // Adding Label and Setting
+        // the Size of the Frame
+        frame.add(label);
+        frame.setSize(300, 300);
+
+        // Making the Frame visible
+        frame.setVisible(true);
+
+        // Using WindowListener for closing the window
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
+
+	}
+
+}
