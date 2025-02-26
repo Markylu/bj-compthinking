05179891990b9678a6ecc946289930380f948aae
Markylu <46211683+Markylu@users.noreply.github.com>
2025-02-26 18:23:26 -0500

    card

--- blackjack/src/Table.java
+++ blackjack/src/Table.java
@@ -4,6 +4,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Table {
+    int tableWidth = 1024;
+    int tableHeight = tableWidth/16*9;
+    int cardheight = 154;
+    int cardwidth = 110;
+
+
     JFrame window = new JFrame("Blackjack");
     JPanel gamePanel = new JPanel(){
         @Override
@@ -11,7 +17,7 @@ public class Table {
             super.paintComponent(g);
             try{
                 Image img = new ImageIcon(getClass().getResource("./cards/BACK.png")).getImage();
-                g.drawImage(img, 500, 500, 100, 150, null);
+                g.drawImage(img, 1024, 600, cardwidth, cardheight, null);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -23,7 +29,7 @@ public class Table {
         window.setResizable(false);
         window.pack();
         window.setLocationRelativeTo(null);
-        window.setSize(1200, 600);
+        window.setSize(tableWidth, tableHeight);
         window.setVisible(true);
         gamePanel.setLayout(new BorderLayout());
         gamePanel.setBackground(new Color(53, 101, 77));
