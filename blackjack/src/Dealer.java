c335588fcc6e6940e68b5b0aa1146eddc58d1e8e
Markylu <46211683+Markylu@users.noreply.github.com>
2025-02-27 10:23:23 -0500

    clean up

--- blackjack/src/Dealer.java
+++ blackjack/src/Dealer.java
@@ -1,15 +1,32 @@
 
 public class Dealer extends Player {
+    Card hiddenCard;
     public Dealer() {
         super();
         playerID = 0;
     }
 
     @Override
-    public void getBets() {
+    public void placeBets() {
         // dealer does not place bets
         System.out.println("Dealer does not place bets");
     }
+
+    @Override
+    public void drawCard() {
+        // dealer draws a card
+        if (hand.isEmpty()){
+            hiddenCard = Deck.draw();
+            hand.add(hiddenCard);
+        }
+        
+        
+        if (card.value.equals("A")) {
+            aceCount++;
+        }
+        handValue += Card.getCardValue(card);
+    }
+
 
 
 }
--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -11,7 +11,7 @@ public class Deck {
     }
 
     public static void newDeck() {
-        deck = new ArrayList<Card>();
+        deck = new ArrayList<>();
         String[] suits = {"H", "D", "C", "S"};
         String[] values = {"A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         for (String suit : suits) {
