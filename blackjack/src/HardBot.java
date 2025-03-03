ce58c24f926f3c349c2a18cc5b775975844bab3f
Thomasarmstrong410 <thomas.armstrong@sps.edu>
2025-03-03 18:10:44 -0500

    Finished the AIs

--- blackjack/src/HardBot.java
+++ blackjack/src/HardBot.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class HardBot extends Player{
     Random random = new Random();
+
+
+    public static double calculateExpectedValue(ArrayList<Card> deck) {
+        double deckValue = 0.0;
+        double cardNumber = 0.0;
+        for (Card card : deck) {
+            if (card.suit.equals("A") && deckValue + 11 <= 21) {
+                deckValue += 11;
+                cardNumber++;
+            } else {
+                deckValue += Card.getCardValue(card);
+                cardNumber++;
+            }
+        }
+        return deckValue/cardNumber;
+    }
 
     public HardBot(){
         super();
@@ -11,21 +28,31 @@ public class HardBot extends Player{
 
     @Override
     public void placeBet(){
-        bet = random.nextInt(0,balance);
+        bet = 3*balance/10;
         status = "Bet Amount: " + bet;
     }
 
     @Override
     public void makeDecision(){
         calculateHandValue();
-        if (handValue<21){
-        int randomNumberInRange = random.nextInt(0, 2);
-        if(randomNumberInRange == 0){
+        if (handValue<16 && HardBot.calculateExpectedValue(Deck.deck)<6.5385){
             hit();
         }
+        else if (handValue<17 && HardBot.calculateExpectedValue(Deck.deck)<6){
+            hit();
+        }
+        else if(handValue<18 && HardBot.calculateExpectedValue(Deck.deck)<5){
+            hit();
+        }
+        else if(handValue<19 && HardBot.calculateExpectedValue(Deck.deck)<4){
+            hit();}
+        else if(handValue<20 && HardBot.calculateExpectedValue(Deck.deck)<3){
+            hit();}
+        else if(handValue<21 && HardBot.calculateExpectedValue(Deck.deck)<2){
+            hit();}
         else{
             stand();
         }
 }
     }
-}
+
